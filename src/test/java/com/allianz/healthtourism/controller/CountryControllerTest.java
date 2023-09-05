61d56c6ef0d3f345f4a8cc63633458f406b36a21
Harun TAŞCI <haruntasci77@gmail.com>
2023-09-05 09:41:04 +0300

    Add getByUUID and deleteByUUID unit tests

--- src/test/java/com/allianz/healthtourism/controller/CountryControllerTest.java
+++ src/test/java/com/allianz/healthtourism/controller/CountryControllerTest.java
@@ -3,6 +3,7 @@ package com.allianz.healthtourism.controller;
 import com.allianz.healthtourism.model.CountryDTO;
 import com.allianz.healthtourism.model.requestDTO.CountryRequestDTO;
 import com.allianz.healthtourism.service.CountryService;
+import com.allianz.healthtourism.util.constants.Constants;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,6 +11,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -46,5 +49,49 @@ class CountryControllerTest {
 
     }
 
+    @Test
+    void getByUUID_shouldGetSuccessfully() {
+        //given
+        CountryDTO expected = new CountryDTO();
+        expected.setName("Germany");
+        when(mockCountryService.getByUUID(any())).thenReturn(expected);
+
+        //when
+        ResponseEntity<CountryDTO> response = underTest.getByUUID(UUID.randomUUID());
+        CountryDTO actual = response.getBody();
+
+        //then
+        assertAll(
+                () -> assertNotNull(actual),
+                () -> assertEquals(HttpStatus.OK, response.getStatusCode()),
+                () -> assertEquals(expected, actual),
+                () -> assertEquals(expected.getName(), actual.getName())
+        );
+    }
+
+
+    @Test
+    void deleteByUUID_shouldDeleteSuccessfully() {
+        when(mockCountryService.deleteByUUID(any())).thenReturn(true);
+
+        ResponseEntity<String> response = underTest.deleteByUUID(UUID.randomUUID());
+
+        assertAll(
+                () -> assertEquals(HttpStatus.OK, response.getStatusCode()),
+                () ->     assertEquals(Constants.DELETE_SUCCESS_MESSAGE, response.getBody())
+        );
+    }
+
+    @Test
+    void deleteByUUID_shouldNotDeleteSuccessfully() {
+        when(mockCountryService.deleteByUUID(any())).thenReturn(false);
+
+        ResponseEntity<String> response = underTest.deleteByUUID(UUID.randomUUID());
+
+        assertAll(
+                () -> assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode()),
+                () ->     assertEquals(Constants.DELETE_ERROR_MESSAGE, response.getBody())
+        );
+    }
 
 }
